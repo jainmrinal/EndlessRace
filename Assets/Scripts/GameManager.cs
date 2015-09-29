13635d278087c8357dc9bfbb1893ed17695ceb4a
Mrinal Jain <mnj8396@rit.edu>
2015-09-29 13:22:42 -0400

    Added dimension switching.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int a;
+    private static GameManager instance = null;
+    private GameManager() { }
+    public static GameManager getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+    public delegate void SwitchDimension();
+    public static event SwitchDimension switchDimension;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        switchDimension();
+    }
+
+   public void CheckSingleTon()
+    {
+        a += 5;
+    }
+}
---
+++ Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleScript : MonoBehaviour {
+    GameManager GM;
+
+    void Start()
+    {
+        GM = GameManager.getInstance();
+        GameManager.switchDimension += SwitchRendererState;
+  
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+           GM.CheckSingleTon();
+            Debug.Log(GM.a);
+        }
+    }
+    void SwitchRendererState()
+    {
+        this.gameObject.GetComponent<Renderer>().enabled ^= true;
+    }
+}
