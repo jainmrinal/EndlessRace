3b09331dc1013fa17e887dc59e6619d050613af1
Mrinal Jain <mnj8396@rit.edu>
2015-09-29 10:35:59 -0400

    Added project.

---
+++ Assets/Scripts/Accelerate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerate : MonoBehaviour {
+
+    public float acceleration = 2.0f;
+    public float maxSpeed = 10.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(0, acceleration * Time.deltaTime, 0);
+        if (acceleration < maxSpeed)
+        {
+            acceleration += 1.0f;
+        }
+	}
+}
