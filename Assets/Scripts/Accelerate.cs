787543aaa73c9df27ecf377b121b0c5402dd9ba5
Srinivasan <tsrivishnu99@github.com>
2015-09-30 16:44:09 -0400

    acceleration updated with intervals.

--- Assets/Scripts/Accelerate.cs
+++ Assets/Scripts/Accelerate.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class Accelerate : MonoBehaviour {
 
-    private float acceleration = 0;
+    public float interval = 10.0f;
+    public float accelerationInterval = 0;
+
+    private float acceleration = 1;
     private float Speed = 10.0f;
     private CharacterController controller;
 	// Use this for initialization
@@ -13,7 +16,7 @@ public class Accelerate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        acceleration = Mathf.Max(0, Mathf.Sin(Time.time/10.0f));
+        acceleration = Mathf.Max(0, Mathf.Sin(Time.time/interval) - ( 1- accelerationInterval));
         Speed += (acceleration * Time.deltaTime);
         controller.SimpleMove(this.transform.forward * Speed);
 	}
