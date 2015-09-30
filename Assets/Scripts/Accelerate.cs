88dd3fe9f556f08f22e4fc54021925216b549ec9
Srinivasan <tsrivishnu99@github.com>
2015-09-30 16:31:31 -0400

    Acceleration Script updated

--- Assets/Scripts/Accelerate.cs
+++ Assets/Scripts/Accelerate.cs
@@ -3,19 +3,18 @@ using System.Collections;
 
 public class Accelerate : MonoBehaviour {
 
-    public float acceleration = 2.0f;
-    public float maxSpeed = 10.0f;
+    private float acceleration = 0;
+    private float Speed = 10.0f;
+    private CharacterController controller;
 	// Use this for initialization
 	void Start () {
-	
+        controller = this.gameObject.GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(0, acceleration * Time.deltaTime, 0);
-        if (acceleration < maxSpeed)
-        {
-            acceleration += 1.0f;
-        }
+        acceleration = Mathf.Max(0, Mathf.Sin(Time.time/10.0f));
+        Speed += (acceleration * Time.deltaTime);
+        controller.SimpleMove(this.transform.forward * Speed);
 	}
 }
